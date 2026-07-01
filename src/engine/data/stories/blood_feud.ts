7e69d5e66d684add864248d5275061574777f04d
Ray <110681032@qq.com>
2026-07-01 19:53:06 +0800

    story

--- src/engine/data/stories/blood_feud.ts
+++ src/engine/data/stories/blood_feud.ts
@@ -4,10 +4,10 @@ export const BLOOD_FEUD: Story = {
     id: 'blood_feud',
     name: '血海深仇',
     characterName: '遗孤',
-    desc: '幼年家族被隐藏boss的组织灭门，会长（父亲挚友）恰好救了你，从此在青山镇长大。',
+    desc: '幼年家族被隐藏boss的组织灭门，会长（父亲挚友）恰好救了你，从此在青山镇长大。你父亲曾在军中服役，与铁剑·红提是同袍。',
     tags: [],
     comment:
-        '一阶段＝一个身负重伤的组织成员｜二阶段＝阿九来照顾会长，你喜欢上了TA；二阶段末发现阿九也是组织的人，错手杀死TA｜三阶段＝打军师和隐藏boss，复仇却填不满心灵缺失，最终选择"回到过去"。',
+        '一阶段＝一个身负重伤的组织成员｜二阶段守门人＝阿九来照顾会长，你喜欢上了TA；二阶段末发现阿九也是组织的人，错手杀死TA｜三阶段＝打军师和隐藏boss，复仇却填不满心灵缺失，最终选择"回到过去"。陆红提：父亲军中旧友（后续加一个关联节点）。',
     getNodeOverride: (nodeIndex) => {
         if (nodeIndex === 2)
             return {
@@ -19,6 +19,7 @@ export const BLOOD_FEUD: Story = {
                 flavorText:
                     '会长教你的是炼炁协会的基础功法，循序渐进，很是耐心。但你修炼时眼神总是很凶，好像要把仇恨都煅进骨子里。',
             }
+        if (nodeIndex === 22) return { bossId: 'ajiu' }
         return undefined
     },
 }
